de8093e129caf6d53de412265bc6ff950d87ac3d
ASEP-PC <saeful.rochmat240302@gmail.com>
2025-09-04 13:21:41 +0700

    Assignment-FE-Saefulrochmat

---
+++ app/components/ProductCard.js
@@ -0,0 +1,50 @@
+import Image from "next/image";
+
+export default function ProductCard({ product }) {
+    return (
+        <>
+            <div className="w-full max-w-xs bg-slate-600 rounded-xl duration-500 hover:scale-105 border-sky-200 hover:shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]">
+                <div className="relative w-full h-48 sm:h-52">
+                    <Image
+                    src={product.image}
+                    fill
+                    sizes="(max-width: 640px) 50vw, (max-width: 1024px) 33vw, 25vw"
+                    alt={product.title}
+                    className="object-cover rounded-t-xl p-4"
+                    />
+                </div>
+                <div className="px-4 py-3">
+                    <span className="text-slate-100 mr-3 uppercase text-xs">
+                    {product.category}
+                    </span>
+                    <p className="text-lg font-bold text-slate-100 truncate block capitalize">
+                    {product.title}
+                    </p>
+                    <div className="flex items-center">
+                    <p className="text-xl font-semibold text-slate-100 cursor-auto my-3">
+                        $ {product.price}
+                    </p>
+                    <del>
+                        <p className="text-sm text-slate-100 cursor-auto ml-2">
+                        $ {product.price}
+                        </p>
+                    </del>
+                    <div className="ml-auto">
+                        <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="20"
+                        fill="currentColor"
+                        className="bi bi-bag-plus"
+                        viewBox="0 0 16 16"
+                        >
+                        <path d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z" />
+                        <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                        </svg>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
---
+++ app/components/ProductList.js
@@ -0,0 +1,157 @@
+'use client'
+
+import { useEffect, useState } from "react";
+import useSWR from "swr";
+import ProductCard from "./ProductCard";
+
+const fetcher = (url) => fetch(url).then((r) => r.json());
+
+export default function ProductList() {
+  const { data, error, isLoading } = useSWR(
+    process.env.NEXT_PUBLIC_URL_API_PRODUCT,
+    fetcher
+  );
+
+  const { data: categories } = useSWR(
+    process.env.NEXT_PUBLIC_URL_API_PRODUCT_CATEGORIES,
+    fetcher
+  );
+
+  // state untuk search
+  const [query, setQuery] = useState("");
+  // query debounce
+  const [debouncedQuery, setDebouncedQuery] = useState("");
+  // query loading Searching
+  const [isSearching, setIsSearching] = useState(false);
+
+  //state untuk kategori
+  const [selectedCategory, setSelectedCategory] = useState ("All");
+  //state untuk sortir harga
+  const [sortedPrice, setSortedPrice] = useState("default");
+
+  // debounce effect
+  useEffect(() => {
+    if (!query) {
+      setDebouncedQuery("");
+      return;
+    }
+
+    setIsSearching(true); // mulai searching
+    const handler = setTimeout(() => {
+      setDebouncedQuery(query);
+      setIsSearching(false); // selesai searching
+    }, 2000); // tunggu 500ms
+
+    return () => clearTimeout(handler);
+  }, [query]);
+
+  if (isLoading) return <div className="flex justify-center items-start min-h-screen pt-32">
+            <div
+                className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+                role="status"
+            >
+                <span className="sr-only">Loading...</span>
+            </div>
+        </div>;
+  if (error) return <div>Error : {error.message}</div>;
+
+  // filter case-insensitive pakai debouncedQuery
+  // filter gabungan
+  let filteredProducts = data.filter((p) => {
+    const matchQuery = p.title.toLowerCase().includes(debouncedQuery.toLowerCase());
+    const matchCategory = selectedCategory === "All" || p.category === selectedCategory;
+    return matchQuery && matchCategory;
+  });
+  //sort produk harga
+  if (sortedPrice === "asc") {
+    filteredProducts =  [...filteredProducts].sort((a, b) => a.price - b.price);
+  } else if (sortedPrice === "desc") {
+    filteredProducts = [...filteredProducts].sort((a, b) => b.price - a.price);
+  };
+
+  return (
+    <>
+        <section className="flex flex-col sm:flex-row justify-center items-center gap-4 sm:gap-6 p-4">
+            {/* Search Bar */}
+            <div className="relative w-full sm:w-1/3">
+                <input
+                type="text"
+                placeholder="Cari produk..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                className="bg-slate-500 h-10 px-5 pr-10 rounded-full text-sm border border-slate-400 w-full focus:outline-none focus:ring-2 focus:ring-blue-400 transition-all duration-300"
+                />
+                <button
+                type="submit"
+                className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-200 hover:text-white"
+                >
+                <svg
+                    className="h-5 w-5"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                >
+                    <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"></path>
+                </svg>
+                </button>
+            </div>
+
+            {/* Filter Kategori */}
+            <div className="w-full sm:w-1/4">
+                <select
+                value={selectedCategory}
+                onChange={(e) => setSelectedCategory(e.target.value)}
+                className="bg-slate-500 border border-slate-400 rounded px-4 py-2 w-full text-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
+                >
+                <option value="All">All</option>
+                {categories &&
+                    categories.map((cat) => (
+                    <option key={cat} value={cat}>
+                        {cat.charAt(0).toUpperCase() + cat.slice(1)}
+                    </option>
+                    ))}
+                </select>
+            </div>
+
+            {/* Sort Harga */}
+            <div className="w-full sm:w-1/4">
+                <select
+                value={sortedPrice}
+                onChange={(e) => setSortedPrice(e.target.value)}
+                className="bg-slate-500 border border-slate-400 rounded px-4 py-2 w-full text-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
+                >
+                <option value="default">Default</option>
+                <option value="asc">Harga: Rendah → Tinggi</option>
+                <option value="desc">Harga: Tinggi → Rendah</option>
+                </select>
+            </div>
+            </section>
+
+
+      {isSearching && (
+        <div className="flex justify-center items-start min-h-screen pt-32">
+            <div
+                className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+                role="status"
+            >
+                <span className="sr-only">Loading...</span>
+            </div>
+        </div>
+      )}
+
+    <section className="w-full max-w-7xl mx-auto px-4 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-y-10 gap-x-6 mt-10 mb-5 justify-items-center">
+    {!isSearching &&
+        (filteredProducts.length > 0 ? (
+        filteredProducts.map((item) => (
+            <ProductCard key={item.id} product={item} />
+        ))
+        ) : (
+        <p className="text-white text-center col-span-full">
+            Produk Tidak Ditemukan
+        </p>
+        ))}
+    </section>
+
+
+    </>
+  );
+}
